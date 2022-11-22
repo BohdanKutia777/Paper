2095c2625754c76fd9423e5d22988cf13e5e4b8a
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2022-11-22 16:02:22 +0200

    done task 3 lesson 18

---
+++ lesson-18/3. Задача с урока/script.js
@@ -0,0 +1,15 @@
+const defer = (func, ms) => {
+    return function () {
+        setTimeout(() => func.apply(this, arguments), ms);
+    }
+}
+
+const user = {
+    name : 'Doe',
+    sayHi () {
+        console.log(`Hi, I'm ${this.name}`);
+    },
+}
+
+let differendHi = defer(user.sayHi, 1000);
+differendHi.call({name : "Bob"});
