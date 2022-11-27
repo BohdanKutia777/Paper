afa2e88a9daddacecd9158cb4613fad9b52d049b
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2022-11-27 22:18:26 +0200

    80% done task 1 lesson 22

--- lesson-22/1. Задача с урока/start/tasks.js
+++ lesson-22/1. Задача с урока/start/tasks.js
@@ -5,3 +5,32 @@ const tasks = [
   { text: 'Visit doctor', done: true },
   { text: 'Buy meat', done: true },
 ];
+const listElem = document.querySelector('.list');
+const renderListIteams = listIteams => {
+
+  const listIteamsElems = listIteams
+  .sort((a,b) => a.done - b.done)
+  .map(({text, done}) => {
+    
+      const listItemElem = document.createElement('li');
+      listItemElem.classList.add('list__item');
+
+      if (done) {
+        listItemElem.classList.add('list__item_done');
+      }
+
+      const chekboxElem = document.createElement('input');
+      chekboxElem.setAttribute('type','checkbox');
+      chekboxElem.checked = done;
+      chekboxElem.classList.add('list__item-checkbox');
+      listItemElem.append(chekboxElem, text);
+
+      return listItemElem;
+  });
+  console.log(Array.from(listIteamsElems));
+  listElem.append(Array.from(listIteamsElems));
+
+};
+
+
+renderListIteams(tasks);
