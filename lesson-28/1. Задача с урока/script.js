830acf2d706415f39e6b0a8155dce5795cde3891
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2022-12-08 17:26:26 +0200

    done task1 lesson28

---
+++ lesson-28/1. Задача с урока/script.js
@@ -0,0 +1,29 @@
+
+
+const addImage =  (ImgSrc, callback) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'Photo');
+    imgElem.src = ImgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+        const {height, width} = imgElem;
+        callback({width,height});
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+};
+
+
+const ImgSrc = 'https://images.prom.ua/2913529835_w640_h640_vafelnaya-kartinka-among.jpg';
+const onImageLoaded = (data) => {
+    const {height, width} = data;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} * ${height}`;
+
+
+};
+
+addImage(ImgSrc,onImageLoaded);
