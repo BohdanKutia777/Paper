a6adb894f9e5082255d4176900e45fcc1d33849f
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2022-12-09 17:08:02 +0200

    done task 2 lesson 30

---
+++ lesson-30/2. Задача с урока/script.js
@@ -0,0 +1,27 @@
+const asyncCalculator = number => new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`Initial value: ${number}`);
+            resolve(number);
+        }, 500);
+    })
+    .then(value => new Promise((resolve) => {
+        setTimeout(() => {
+        let result = value * value;
+        console.log(`Squared value: ${result}`);
+        resolve(result)
+        }, 500);
+    }))
+    .then(value => new Promise ((resolve)=> {
+        setTimeout(() => {
+        let result = value * 2;
+        console.log(`Doubled value: ${result}`);
+        resolve(result)   
+        }, 500);
+    }))
+
+
+asyncCalculator(5)
+    .then(data => console.log(data));
+
+
+    
