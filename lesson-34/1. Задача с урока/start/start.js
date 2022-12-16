90f19f4ef3e646eb940abba34bd4c370e2bf10d9
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2022-12-16 19:08:42 +0200

    done task 1 lesson 34

--- lesson-34/1. Задача с урока/start/start.js
+++ lesson-34/1. Задача с урока/start/start.js
@@ -5,7 +5,7 @@ window.addEventListener("error", function onUnhandledError (err) {
 let userParsingResult;
 
 try {
-    const user = JSON.parse(/* валидный JSON c данными пользователя */);
+    const user = JSON.parse('{"name": "John"}');
     console.log("User data: ", user);
     userParsingResult = "success";
 } catch(e) {
@@ -17,10 +17,10 @@ try {
 let productParsingResult;
 
 try {
-    const product = JSON.parse(/* сломанный JSON c данными о продукте */);
+    const product = JSON.parse({"name": "John"});
     console.log("Product data: ", product);
     productParsingResult = "success";
-} catch(e) {s
+} catch(e) {
     productParsingResult = "error";
 } finally {
     console.log(`Product parsing finished with ${productParsingResult}`);
