7307f7f4934ecf60ad85737064e3587e000ae871
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-02-15 15:15:12 +0200

    download lesson-47

---
+++ lesson-36/2. Task start/src/scripts/Alphabet symmetry.js
@@ -0,0 +1,4 @@
+const solve = arr => {  
+    const alphabeth = "abcdefghijklmnopqrstuvwxyz";
+    return arr.map(x => x.toLowerCase().split('').filter((y,i) => i==alphabeth.indexOf(y)).length);
+  };
